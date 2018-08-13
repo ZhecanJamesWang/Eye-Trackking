a6581c93fc28a9747e4a4eec458971808cff8a48
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2018-08-13 17:49:50 +0800

    update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     parser.add_argument('-train', action='store_true', help='train flag')
     parser.add_argument('-test', action='store_true', help='train flag')
     parser.add_argument('-data', type=str, default='small', help='which dataset, small or big')
-    parser.add_argument('-max_epoch', '--max_epoch', type=int, default=10, help='max number of iterations (default 100)')
+    parser.add_argument('-max_epoch', '--max_epoch', type=int, default=100, help='max number of iterations (default 100)')
     parser.add_argument('-batch_size', type=int, default=100, help='batch size (default 50)')
     parser.add_argument('-patience', type=int, default=30, help='early stopping patience (default 10)')
     parser.add_argument('-dev', type=str, default="-1", help='what cpu or gpu (recommended) use to train the model')
