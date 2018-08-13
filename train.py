f05dbaca00fb370a72f0d6dd5c8e9e8a87259870
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2018-08-13 15:40:01 +0800

    update

--- train.py
+++ train.py
@@ -123,7 +123,7 @@ def train(args):
             validation_data=generator_val_data(val_names, dataset_path, batch_size, img_ch, img_cols, img_rows),
             validation_steps=(len(val_names)) / batch_size,
             callbacks=[EarlyStopping(patience=patience),
-                       ModelCheckpoint("weights_big/weights.{epoch:03d}-{val_loss:.5f}.hdf5", save_best_only=True)
+                       ModelCheckpoint("weights_big/weights.{epoch:03d}-{val_loss:.5f}.hdf5", verbose=1, save_best_only=True, mode='auto', period=0.1)
                        ]
         )
 
